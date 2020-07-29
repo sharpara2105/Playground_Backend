9b4c90a78af589a2a1e394291d79feacea4903d6
sharpara2105 <sharpara.2105@gmail.com>
2020-07-30 01:16:11 +0530

    till post

--- express-server.js
+++ express-server.js
@@ -1,17 +1,9 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-// and now this app object has all the crud operators that we can use : 
-
-const port = process.env.PORT || 3000;
-
-app.get('/',(req,res) => {
-    res.send('zakabam');   //this call back function here is also called 'Route Handler'
-});
-app.get('/api/clothing',(req,res) => {
-    res.send(['t-shirt','shirt','pants']);   //this call back function here is also called 'Route Handler'
-});
 const products = [
     {product_code: 1000,
     product_name : 'unicorn',
@@ -28,6 +20,17 @@ const products = [
     Available : 'yes'
 },
 ]
+// and now this app object has all the crud operators that we can use : 
+
+const port = process.env.PORT || 4000;
+//read operation : get
+app.get('/',(req,res) => {
+    res.send('zakabam');   //this call back function here is also called 'Route Handler'
+});
+app.get('/api/clothing',(req,res) => {
+    res.send(['t-shirt','shirt','pants']);   //this call back function here is also called 'Route Handler'
+});
+
 app.get('/api/clothing/:id',(req,res) => {
     const product = products.find(c => c.product_code === parseInt(req.params.id)); //req.params.id returns a string so we need to convert it to int for comparison.
     if(!product) 
@@ -35,5 +38,33 @@ app.get('/api/clothing/:id',(req,res) => {
     
     res.send(product);   //this call back function here is also called 'Route Handler'
 });
+
+
+// create opearation : post
+
+app.post('/api/clothing',(req,res) => {
+    const schema = Joi.object({
+        product_name : Joi.string().min(3).required()
+    });
+    const p = {product_code: 1002,
+                product_name : req.body.product_name,
+                category: {gender : 'female',occassion:'all', type: 'shirt'},
+                price : 1500,
+                currency : 'INR',
+                Available : 'yes'
+    }
+    // const result = Joi.validate(req.body,schema);
+    const result = schema.validate(req.body);
+    console.log(result);
+    if(result.error) {
+        res.status(400).send(result.error);
+        return;
+    }
+    products.push(p);
+    res.send(p);
+    // res.json(req.body)
+})
+
+
 app.listen(port,()=> console.log(`Listening on port ${port}`));
 
